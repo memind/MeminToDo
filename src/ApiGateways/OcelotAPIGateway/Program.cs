6f01746406063270f9c82354d1bea3adcb36a7df
Mehmet Emin Dağ <mehmetemin.dag@bilgeadamboost.com>
2023-09-09 22:53:06 +0300

    - Added Gateway integrations and rate limiting on Ocelot

--- src/ApiGateways/OcelotAPIGateway/Program.cs
+++ src/ApiGateways/OcelotAPIGateway/Program.cs
@@ -1,6 +1,16 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration)
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
