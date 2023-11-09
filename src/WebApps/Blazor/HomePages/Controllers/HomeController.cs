de72658c36db2fd74086337742e6ce156d61cd3b
Mehmet Emin Dağ <rock_med99@hotmail.com>
2023-11-09 21:33:47 +0300

    - New Access Token operations via Refresh Token

--- src/WebApps/Blazor/HomePages/Controllers/HomeController.cs
+++ src/WebApps/Blazor/HomePages/Controllers/HomeController.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System.Diagnostics;
 
 namespace HomePages.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
@@ -32,6 +34,61 @@ namespace HomePages.Controllers
 
             Console.WriteLine("------------------------------------");
             return View();
+        }
+
+
+
+        public async Task<IActionResult> NewAccessTokenTest() 
+        {
+            AuthenticateResult authenticateResult = await HttpContext.AuthenticateAsync();
+            IOrderedEnumerable<KeyValuePair<string, string>> properties = authenticateResult.Properties.Items.OrderBy(p => p.Key);
+            return View(properties);
+        }
+        public async Task<IActionResult> NewAccessToken()
+        {
+            string refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            HttpClient httpClient = new HttpClient();
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest()
+            {
+                ClientId = "MeminToDoHome",
+                ClientSecret = "memintodohome",
+                RefreshToken = refreshToken,
+                Address = (await httpClient.GetDiscoveryDocumentAsync("https://localhost:8005")).TokenEndpoint
+            };
+
+            TokenResponse tokenResponse = await httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+            AuthenticationProperties properties = (await HttpContext.AuthenticateAsync()).Properties;
+
+            properties.StoreTokens(
+                new List<AuthenticationToken> {
+                    new AuthenticationToken
+                    {
+                        Name = OpenIdConnectParameterNames.IdToken,
+                        Value = tokenResponse.IdentityToken
+                    },
+
+                    new AuthenticationToken
+                    {
+                        Name = OpenIdConnectParameterNames.AccessToken,
+                        Value = tokenResponse.AccessToken
+                    },
+
+                    new AuthenticationToken
+                    {
+                        Name = OpenIdConnectParameterNames.RefreshToken,
+                        Value = tokenResponse.RefreshToken
+                    },
+
+                    new AuthenticationToken
+                    {
+                        Name = OpenIdConnectParameterNames.ExpiresIn,
+                        Value = DateTime.UtcNow.AddSeconds(tokenResponse.ExpiresIn).ToString("O")
+                    }
+                });
+
+            await HttpContext.SignInAsync("MeminToDoHomeCookie", (await HttpContext.AuthenticateAsync()).Principal, properties);
+            return RedirectToAction(nameof(HomeController.NewAccessTokenTest));
         }
     }
 }
